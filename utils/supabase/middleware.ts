412c6ade052e897d2046f96aec523107e3337d2f
Barış Zerk <bariszerk@gmail.com>
2025-06-26 22:28:53 +0300

    Update middleware.ts

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -43,7 +43,10 @@ export async function updateSession(request: NextRequest) {
 		!user &&
 		!request.nextUrl.pathname.startsWith('/login') &&
 		!request.nextUrl.pathname.startsWith('/auth') &&
-		!request.nextUrl.pathname.startsWith('/signup')
+		!request.nextUrl.pathname.startsWith('/signup') &&
+		!request.nextUrl.pathname.startsWith('/forgot-password') &&
+		!request.nextUrl.pathname.startsWith('/verify-email') &&
+		!request.nextUrl.pathname.startsWith('/update-password')
 	) {
 		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone();
